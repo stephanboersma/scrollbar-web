f71bde4764026a5c22fe7cd5ec2df959ff4a9aae
Mathias Fink <matf@itu.dk>
2022-01-30 13:46:21 +0100

    Removed previous shifts added myshifts, upforgrabs

--- src/pages/ShiftPlan/index.js
+++ src/pages/ShiftPlan/index.js
@@ -1,5 +1,4 @@
-import { Checkbox, Collapse, DatePicker, Divider, message, Space } from 'antd';
-import moment from 'moment';
+import { Divider, message, Space } from 'antd';
 import React, { useContext, useState } from 'react';
 
 import AuthContext from '../../contexts/AuthContext';
@@ -8,7 +7,8 @@ import EventContext from '../../contexts/EventContext';
 import ShiftContext from '../../contexts/ShiftContext';
 import TendersContext from '../../contexts/TendersContext';
 import { Text } from '../../styles/atoms/Typography';
-import EventListItem from '../../styles/molecules/EventListItem';
+import FilteredShifts from '../../styles/molecules/FilteredShifts';
+import FilteredShiftsRadioGroup from '../../styles/molecules/FilteredShiftsRadioGroup';
 import SideBarPage from '../../styles/templates/SideBarPage';
 
 const ShiftPlan = () => {
@@ -19,21 +19,13 @@ const ShiftPlan = () => {
     EngagementContext
   );
 
-  const { Panel } = Collapse;
+  const [mode, setMode] = useState('all');
 
   const { user } = useContext(AuthContext);
 
-  const [myShiftChecked, setMyShiftChecked] = useState(false);
-  const [showPreviousShifts, setShowPreviousShifts] = useState(false);
-  const [shiftFrom, setShiftFrom] = useState(moment().subtract(1, 'months'));
-
-  const getEventEngagements = (eventId) => {
-    return engagementState.engagements.filter(
-      (engagement) => engagement.eventId === eventId
-    );
-  };
-  const getEventShifts = (eventId) => {
-    return shiftState.shifts.filter((shift) => shift.eventId === eventId);
+  const handleModeChange = (e) => {
+    const mode = e.target.value;
+    setMode(mode);
   };
 
   const onTakeShift = (engagement, userId) => {
@@ -54,73 +46,28 @@ const ShiftPlan = () => {
       .catch((error) => message.error('An error occurred: ' + error.message));
   };
 
-  const filterEventsWithUser = (events, userId) => {
-    var eventidsWithUser = engagementState.engagements
-      .filter((engagement) => engagement.userId === userId)
-      .map((e) => e.eventId);
-    return events.filter((_event) => eventidsWithUser.includes(_event.id));
-  };
-
   return (
     <SideBarPage title="Tender site">
-      <Collapse
-        title="Filters"
-        direction="vertical"
-        style={{ width: '100%' }}
-        ghost
-        bordered
-      >
-        <Panel header="Filters">
-          <Checkbox
-            checked={myShiftChecked}
-            onChange={(e) => setMyShiftChecked(e.target.checked)}
-          >
-            Show events where I got a shift
-          </Checkbox>
-          <Checkbox
-            checked={showPreviousShifts}
-            onChange={(e) => setShowPreviousShifts(e.target.checked)}
-          >
-            Show previous events from
-          </Checkbox>
-          <DatePicker
-            value={shiftFrom}
-            onChange={(date) =>
-              date ? setShiftFrom(date) : setShiftFrom(moment())
-            }
-          ></DatePicker>
-        </Panel>
-      </Collapse>
+      <FilteredShiftsRadioGroup
+        handleModeChange={handleModeChange}
+        mode={mode}
+        userId={user.id}
+        shifts={shiftState.shifts}
+        engagements={engagementState.engagements}
+      ></FilteredShiftsRadioGroup>
       <Divider></Divider>
       <Space direction="vertical" style={{ width: '100%' }}>
         {eventState.events.filter((_event) => _event).length > 0 ? (
-          (showPreviousShifts
-            ? myShiftChecked
-              ? filterEventsWithUser(
-                  eventState.previousEvents
-                    .filter((e) => e.start.toDate() >= shiftFrom.toDate())
-                    .concat(eventState.events),
-                  user.id
-                )
-              : eventState.previousEvents
-                  .filter((e) => e.start.toDate() >= shiftFrom.toDate())
-                  .concat(eventState.events)
-            : myShiftChecked
-            ? filterEventsWithUser(eventState.events, user.id)
-            : eventState.events
-          ).map((each, i) => {
-            return (
-              <EventListItem
-                event={each}
-                engagements={getEventEngagements(each.id)}
-                shifts={getEventShifts(each.id)}
-                key={i}
-                users={tenderState.tenders}
-                onTakeShift={onTakeShift}
-                setUpForGrabs={onSetUpForGrabs}
-              />
-            );
-          })
+          <FilteredShifts
+            mode={mode}
+            userId={user.id}
+            shifts={shiftState.shifts}
+            events={eventState.events}
+            engagements={engagementState.engagements}
+            tenders={tenderState.tenders}
+            onTakeShift={onTakeShift}
+            onSetUpForGrabs={onSetUpForGrabs}
+          ></FilteredShifts>
         ) : (
           <Text type="secondary">
             No events are currently planned. Stay tuned
